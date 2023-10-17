a63085f96920dfbe2add320e50e05cbce7a3be01
Arjun.M <61102205+Arjunpixel28@users.noreply.github.com>
2023-10-17 11:40:50 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,24 @@
+# Face Up and Downward Counter
+
+This project uses the MediaPipe library to detect faces in a video stream and count the number of times each face moves up or down. It provides a real-time count of upward and downward movements for each detected face.
+
+## Prerequisites
+
+- Python 3.6 or higher
+- Install the required packages using `pip install -r requirements.txt`
+
+## Usage
+
+1. Clone the repository:
+
+2. Install dependencies:
+
+3. Run the application:
+
+4. Press `q` to exit the application.
+
+## Notes
+
+- Ensure your camera is properly connected and accessible.
+- Adjust the detection confidence and movement thresholds in the `face_up_down_counter.py` file according to your needs.
+
---
+++ countblock.py
@@ -0,0 +1,67 @@
+import cv2
+import mediapipe as mp
+
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
+
+# Initialize MediaPipe Face Detection module
+face_detection = mp_face_detection.FaceDetection(min_detection_confidence=0.2)
+
+cap = cv2.VideoCapture('rtsp://admin:admin@192.168.0.138')
+face_states = {}  # Dictionary to store face states (up, down)
+up_count = 0  # Counter for upward movements
+down_count = 0  # Counter for downward movements
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Convert the image to RGB format
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Perform face detection
+    results_detection = face_detection.process(rgb_frame)
+
+    if results_detection.detections:
+        for idx, detection in enumerate(results_detection.detections):
+            bboxC = detection.location_data.relative_bounding_box
+            ih, iw, _ = frame.shape
+            bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), int(bboxC.width * iw), int(bboxC.height * ih)
+
+            face_id = idx  # Use the index as a unique ID for each detected face
+
+            # Set initial head position and state for each face
+            if face_id not in face_states:
+                face_states[face_id] = {
+                    'head_position': int(bboxC.ymin * ih) + int(bboxC.height * ih / 2),
+                    'state': None
+                }
+
+            nose_y = int(bboxC.ymin * ih) + int(bboxC.height * ih / 2)
+            
+            # Determine head state (up or down) based on the change in nose position
+            if nose_y < face_states[face_id]['head_position'] - 5:
+                if face_states[face_id]['state'] != 'up':
+                    face_states[face_id]['state'] = 'up'
+                    up_count += 1  # Increment the upward movement counter
+            elif nose_y > face_states[face_id]['head_position'] + 5:
+                if face_states[face_id]['state'] != 'down':
+                    face_states[face_id]['state'] = 'down'
+                    #down_count += 1  # Increment the downward movement counter
+            face_states[face_id]['head_position'] = nose_y  # Update head position for the next iteration
+
+            # Draw bounding box and keypoints on the frame
+            mp_drawing.draw_detection(frame, detection)
+            cv2.putText(frame, f'Face {int(face_id)} State: {face_states[face_id]["state"]}', (bbox[0], bbox[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    # Display the frame with bounding boxes and face states
+    cv2.putText(frame, f'Upward Count: {up_count}', (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.putText(frame, f'Downward Count: {down_count}', (20, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.imshow('Face Detection', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
